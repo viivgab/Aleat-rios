49ea898b73f2d12018181691560e30856342a1a3
viivgab <viivgab@gmail.com>
2026-01-24 13:00:58 -0300

    Mais aulas de Linguagem C

---
+++ linguagem-c/aula2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+  int idade = 0;
+  float peso = 0.0;
+
+  printf("Digite uma idade:\n");
+  scanf("%d", &idade);
+
+  printf("DIgite um peso:\n");
+  scanf("%f", &peso);
+
+  printf("Idade informada: %d.\n", idade);
+  printf("Peso informado: %f.\n", peso);
+
+}
